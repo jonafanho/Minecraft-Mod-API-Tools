f723e0bba328d4e76e4339daff650c57d74212c4
Jonathan Ho <jonafanho@gmail.com>
2022-09-14 11:20:32 -0700

    Added properties to ModProvider

--- src/main/java/com/jonafanho/apitools/MinecraftModAPITools.java
+++
@@ -1,9 +0,0 @@
-package com.jonafanho.apitools;
-
-public class MinecraftModAPITools {
-
-	public static void main(String[] args) {
-		if (args.length > 0) {
-		}
-	}
-}
--- src/main/java/com/jonafanho/apitools/ModProvider.java
+++ src/main/java/com/jonafanho/apitools/ModProvider.java
@@ -1,5 +1,13 @@
 package com.jonafanho.apitools;
 
 public enum ModProvider {
-	CURSE_FORGE, MODRINTH;
+	CURSE_FORGE("curseforge", "CurseForge"), MODRINTH("modrinth", "Modrinth");
+
+	public final String id;
+	public final String name;
+
+	ModProvider(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 }
